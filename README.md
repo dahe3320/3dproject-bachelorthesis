5aaf35698a3e0d6b4658315055fabadd554c8aac
Daniel Hed <dh222tw@student.lnu.se>
2024-01-16 15:10:02 +0100

    modifier of 3d model completed

---
+++ README.md
@@ -0,0 +1,4 @@
+Additional kravspec: 
+1. Mockup collection asset (grid-productimages)
+2. import { GUI } from 'dat.gui' ?
+3. import Stats from 'three/examples/jsm/libs/stats.module' ? 
--- src/App.jsx
+++ src/App.jsx
@@ -7,47 +7,68 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { TextureLoader } from 'three/src/loaders/TextureLoader';
 
 function Studio() {
-  const { boxColour, depth } = useControls({
-    boxColour: "blue",
+
+  const { boxColour, depth, position } = useControls({
+    boxColour: "orange",
     depth: {
       value: 1,
       min: 0,
       max: 5,
       step: 1,
     },
+    position: {
+      value: [1, 0, 2], // Default position
+    },
   });
   
   return (
-    <mesh>
+    <mesh position={position}>
         <boxGeometry args={[1, 1, depth]} />
         <meshStandardMaterial color={boxColour} />
     </mesh>
   )
 }
 
-function Car() {
+
+function Module() {
   const [model, setModel] = useState(null);
 
   useEffect(() => {
     const loader = new GLTFLoader().setPath('notebook/');
     const textureLoader = new TextureLoader();
-    const texture = textureLoader.load('src/assets/mumin.png'); // Replace with the path to your texture
+    const texture = textureLoader.load('src/assets/natureglobe.jpg');
     loader.load(
       'scene.gltf',
       (gltf) => {
         const loadedModel = gltf.scene;
-        //console.log(loadedModel);
-        
+        console.log(loadedModel);
+        //Spiral_Notebook_Spiral_Chome_0
+
         // Find the object by name
         const notebookCover = loadedModel.getObjectByName('Spiral_Notebook_Spiral_Notebook_Cover_0');
+        // notebookCover.traverse(node => {
+
+        //   if(node.isMesh) {  
+        //     node.material.color.set(0xffffff);
+        //     node.material.map = texture;
+        //     node.material.map.repeat.set( 2, 2 );
+        //     console.log(node);
+        //    }
+        //   })
         if (notebookCover) {
+          notebookCover.material.color.set(0xffffff);
           notebookCover.material.map = texture;
-          console.log(notebookCover.material.map);
-          notebookCover.material.map.repeat.set( 2, 2 );
-          notebookCover.material.map.flipY = false;
+          //console.log(notebookCover.material);
+          notebookCover.material.map.repeat.set( 2, 2);
           notebookCover.material.needsUpdate = true;
         }
-        loadedModel.rotation.x = 4;
+
+        const notebookSpiral = loadedModel.getObjectByName('Spiral_Notebook_Spiral_Chome_0');
+        if (notebookSpiral) {
+            notebookSpiral.material.metalness = 0.4;
+            notebookSpiral.material.color.set(0xffa5bb);
+        }
+        //loadedModel.rotation.x = 4;
         setModel(loadedModel);
       },
       undefined,
@@ -68,8 +89,8 @@ function App() {
       <Stars />
       <ambientLight intensity={2} />
       <pointLight position={[10, 10, 10]} />
-      {/* <Studio /> */}
-      <Car />
+      <Studio />
+      <Module />
       </Canvas>
   )
 }
