cd644c8e61392c7ff69cce94ce74f94c6e3dd270
Daniel Hed <dh222tw@student.lnu.se>
2024-01-21 16:59:39 +0100

    Generated model

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Additional kravspec:
 1. Mockup collection asset (grid-productimages)
 2. import { GUI } from 'dat.gui' ?
 3. import Stats from 'three/examples/jsm/libs/stats.module' ? 
+
+
+Background ideas: 
+    Burde office stairs
+    Table with different tableclothes (wood, marble, patterns)
--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,44 @@
-import { useEffect, useState, useRef } from 'react'
+import { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Stars } from "@react-three/drei";
 import './App.css'
-import { useControls } from "leva";
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { TextureLoader } from 'three/src/loaders/TextureLoader';
+// import { useControls } from "leva";
 
 
-function Studio() {
 
-  const { boxColour, depth, position } = useControls({
-    boxColour: "orange",
-    depth: {
-      value: 1,
-      min: 0,
-      max: 5,
-      step: 1,
-    },
-    position: {
-      value: [1, 0, 2], // Default position
-    },
-  });
+// function Studio() {
+
+//   const { boxColour, depth, position } = useControls({
+//     boxColour: "orange",
+//     depth: {
+//       value: 1,
+//       min: 0,
+//       max: 5,
+//       step: 1,
+//     },
+//     position: {
+//       value: [1, 0, 2], // Default position
+//     },
+//   });
   
-  return (
-    <mesh position={position}>
-        <boxGeometry args={[1, 1, depth]} />
-        <meshStandardMaterial color={boxColour} />
-    </mesh>
-  )
-}
+//   return (
+//     <mesh position={position}>
+//         <boxGeometry args={[1, 1, depth]} />
+//         <meshStandardMaterial color={boxColour} />
+//     </mesh>
+//   )
+// }
 
 
-function Module() {
-  const [model, setModel] = useState(null);
 
-  useEffect(() => {
-    const loader = new GLTFLoader().setPath('notebook/');
-    const textureLoader = new TextureLoader();
-    const texture = textureLoader.load('src/assets/natureglobe.jpg');
-    loader.load(
-      'scene.gltf',
-      (gltf) => {
-        const loadedModel = gltf.scene;
-        console.log(loadedModel);
-        //Spiral_Notebook_Spiral_Chome_0
-
-        // Find the object by name
-        const notebookCover = loadedModel.getObjectByName('Spiral_Notebook_Spiral_Notebook_Cover_0');
-        
-        // notebookCover.traverse(node => {
-        //   if(node.isMesh) {  
-        //     node.material.color.set(0xffffff);
-        //     node.material.map = texture;
-        //     node.material.map.repeat.set( 2, 2 );
-        //     console.log(node);
-        //    }
-        //   })
-        if (notebookCover) {
-          notebookCover.material.color.set(0xffffff);
-          notebookCover.material.map = texture;
-          //console.log(notebookCover.material);
-          notebookCover.material.map.repeat.set( 2, 2);
-          notebookCover.material.needsUpdate = true;
-        }
-
-        const notebookSpiral = loadedModel.getObjectByName('Spiral_Notebook_Spiral_Chome_0');
-        if (notebookSpiral) {
-            notebookSpiral.material.metalness = 0.4;
-            notebookSpiral.material.color.set(0xffa5bb);
-        }
-        //loadedModel.rotation.x = 4;
-        setModel(loadedModel);
-      },
-      undefined,
-      function (error) {
-        console.error(error);
-      }
-    );
-  }, []); // Empty dependency array ensures this effect runs once on mount
-
-  return model ? <primitive object={model} /> : null;
-}
 
 function App() {
   const [count, setCount] = useState(0)
   return (  
-      <Canvas>
-      <OrbitControls />
-      <Stars />
+      <Canvas dpr={[1,2]}>
+        <color attach="background" args={["#404040"]} />
+        <fog attach="fog" args={["#213547", 10, 20]} />
       <ambientLight intensity={2} />
       <pointLight position={[10, 10, 10]} />
-      <Studio />
-      <Module />
       </Canvas>
   )
 }
